175d5ee7e55ec5e0f7a9a4980e32c453359bb1d8
Chris Gaaju <chrisgaajukpeme2027@gmail.com>
2026-07-31 23:56:37 +0100

    Update uptime.js

--- bot/gaaju-cmds/utility/uptime.js
+++ bot/gaaju-cmds/utility/uptime.js
@@ -1,50 +1,52 @@
 module.exports = {
-  name: "uptime",
-  aliases: ["up", "runtime"],
-  description: "Show how long the bot has been running",
-  category: "utility",
+name: "uptime",
+aliases: ["up", "runtime"],
+description: "Show how long the bot has been running",
+category: "utility",
 
-  async execute(_0x9d31e, _0x8e593c, _0x4abf4d, _0xe49667, _0x1249d6) {
+async execute(_0x9d31e, _0x8e593c, _0x4abf4d, _0xe49667, _0x1249d6) {
 
-    const _0x1cadeb = _0x8e593c.key.remoteJid;
+const _0x1cadeb = _0x8e593c.key.remoteJid;  
 
-    try {
-      await _0x9d31e.sendMessage(_0x1cadeb, {
-        react: {
-          text: "⏱️",
-          key: _0x8e593c.key
-        }
-      });
-    } catch {}
+try {  
+  await _0x9d31e.sendMessage(_0x1cadeb, {  
+    react: {  
+      text: "⏱️",  
+      key: _0x8e593c.key  
+    }  
+  });  
+} catch {}  
 
-    const _0x80fd31 = process.uptime();
+const _0x80fd31 = process.uptime();  
 
-    const _0x2b6d98 = Math.floor(_0x80fd31 / 86400);
-    const _0x253a7a = Math.floor((_0x80fd31 % 86400) / 3600);
-    const _0xc8e476 = Math.floor((_0x80fd31 % 3600) / 60);
-    const _0x39e2d7 = Math.floor(_0x80fd31 % 60);
+const _0x2b6d98 = Math.floor(_0x80fd31 / 86400);  
+const _0x253a7a = Math.floor((_0x80fd31 % 86400) / 3600);  
+const _0xc8e476 = Math.floor((_0x80fd31 % 3600) / 60);  
+const _0x39e2d7 = Math.floor(_0x80fd31 % 60);  
 
-    const _0xbd8bb2 = [];
+const _0xbd8bb2 = [];  
 
-    if (_0x2b6d98) _0xbd8bb2.push(_0x2b6d98 + "d");
-    if (_0x253a7a) _0xbd8bb2.push(_0x253a7a + "h");
-    if (_0xc8e476) _0xbd8bb2.push(_0xc8e476 + "m");
+if (_0x2b6d98) _0xbd8bb2.push(_0x2b6d98 + "d");  
+if (_0x253a7a) _0xbd8bb2.push(_0x253a7a + "h");  
+if (_0xc8e476) _0xbd8bb2.push(_0xc8e476 + "m");  
 
-    _0xbd8bb2.push(_0x39e2d7 + "s");
+_0xbd8bb2.push(_0x39e2d7 + "s");  
 
-    await _0x9d31e.sendMessage(
-      _0x1cadeb,
-      {
-        text: `╭━━━〔 ⏱️ UPTIME 〕━━━⬣
+await _0x9d31e.sendMessage(  
+  _0x1cadeb,  
+  {  
+    text: `╭━━━〔 ⏱️ UPTIME 〕━━━⬣
+
 ┃
 ┃ ✦ Runtime : ${_0xbd8bb2.join(" ")}
 ┃
-`╰━━━━━━〔 GAAJU-MD ULTRA 〕⬣`,
+╰━━━━━━━━━━━━━━━〔 GAAJU-MD ULTRA 〕⬣`,
 
-             },
-      {
-        quoted: _0x8e593c
-      }
-    );
-  }
+},  
+  {  
+    quoted: _0x8e593c  
+  }  
+);
+
+}
 };
